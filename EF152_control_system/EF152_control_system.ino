8398978536450f81431a3c5909fd502796c73740
Crestor123 <LNinten@gmail.com>
2015-04-20 11:14:41 -0400

    Added moistureSensor

--- EF152_control_system/EF152_control_system.ino
+++ EF152_control_system/EF152_control_system.ino
@@ -9,15 +9,43 @@ void setup()
     
 void loop()
 {
-  photoResistor();
+  counter = millis();
+  if (counter >lastTime+10)
+  {
+    photoResistor();
+    moistureSensor();
+    
+    lastTime = counter;
+  }
 }
 
 int photoResistor()
 {
-  counter = millis();
-  if (counter >lastTime+10){
   int sensorVal = analogRead(A0);
   Serial.println(sensorVal);
-  lastTime = counter;
+}
+
+bool moistureSensor()
+{
+  moisture_val = analogRead(moistureSensor);
+  Serial.println("Moisture sensor reads: ");
+  Serial.println("moisture_val");
+  
+  if (moisture_val >= 950)
+  {
+    Serial.println("Moisture sensor disconnected)
+  }
+  else if (moisture_val >= 900)
+  {
+    Serial.printlin("Soil is above the correct moisture level")
+  }
+  else if (moisture_val >= 850)
+  {
+    Seral.println("Soil is at the correct moisture level");
+  }
+  else if (moisture_val >= 800)
+  {
+    Serial.println("Soil is below the correct moisture level");
   }
 }
+
